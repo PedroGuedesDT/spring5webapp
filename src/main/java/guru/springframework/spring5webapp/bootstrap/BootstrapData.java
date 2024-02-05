ca899cb2ba9858108eaebc4034c85e12140dae91
PedroGuedesDT <159037242+PedroGuedesDT@users.noreply.github.com>
2024-02-05 20:04:23 +0000

    Section 2 - Task2

--- src/main/java/guru/springframework/spring5webapp/bootstrap/BootstrapData.java
+++ src/main/java/guru/springframework/spring5webapp/bootstrap/BootstrapData.java
@@ -2,8 +2,10 @@ package guru.springframework.spring5webapp.bootstrap;
 
 import guru.springframework.spring5webapp.domain.Author;
 import guru.springframework.spring5webapp.domain.Book;
+import guru.springframework.spring5webapp.domain.Publisher;
 import guru.springframework.spring5webapp.repositories.AuthorRepository;
 import guru.springframework.spring5webapp.repositories.BookRepository;
+import guru.springframework.spring5webapp.repositories.PublisherRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -14,9 +16,14 @@ public class BootstrapData implements CommandLineRunner {
 
     private final AuthorRepository authorRepository;
     private final BookRepository bookRepository;
+    private final PublisherRepository publisherRepository;
 
     @Override
     public void run(String... args) throws Exception {
+
+        Publisher asa = new Publisher("ASA", "Streat of Philadelphia", "Philadelphia", "Philadelphia", "123456789");
+        publisherRepository.save(asa);
+
         Author peter = new Author("Pedro", "Guedes");
         Book ddd = new Book("Domain Driven Design", "123456789");
         peter.getBooks().add(ddd);
@@ -34,6 +41,7 @@ public class BootstrapData implements CommandLineRunner {
         bookRepository.save(midnightTrain);
 
         System.out.println("Started in boostrap!");
+        System.out.println("Publishers: " + publisherRepository.count());
         System.out.println("Number of books: " + bookRepository.count());
     }
 }
---
+++ src/main/java/guru/springframework/spring5webapp/domain/Publisher.java
@@ -0,0 +1,51 @@
+package guru.springframework.spring5webapp.domain;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class Publisher {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "publisher_id")
+    private Long id;
+
+    private String name;
+    private String address;
+    private String city;
+    private String state;
+    private String zipCode;
+
+    public Publisher(String name, String address, String city, String state, String zipCode) {
+        this.name = name;
+        this.address = address;
+        this.city = city;
+        this.state = state;
+        this.zipCode = zipCode;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Publisher publisher = (Publisher) o;
+
+        return Objects.equals(id, publisher.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
+    }
+}
